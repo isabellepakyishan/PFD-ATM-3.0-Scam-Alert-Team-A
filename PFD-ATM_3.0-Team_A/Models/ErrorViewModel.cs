29bbbcf5e9722f8a93bfabd03ec75afa8d191293
Ubertronian04 <S10223002@connect.np.edu.sg>
2022-10-31 14:38:26 +0800

    Add project files.

---
+++ PFD-ATM_3.0-Team_A/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PFD_ATM_3._0_Team_A.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
