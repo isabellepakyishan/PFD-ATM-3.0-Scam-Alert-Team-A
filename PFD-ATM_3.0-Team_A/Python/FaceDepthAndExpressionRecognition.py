27ec01f940e4df7f4a8c5b89f085eab74e08c2f2
Ubertronian04 <S10223002@connect.np.edu.sg>
2022-11-14 03:13:45 +0800

    fixed modal and python screen displays for FER and face depth measurement

--- PFD-ATM_3.0-Team_A/Python/FaceDepthAndExpressionRecognition.py
+++ PFD-ATM_3.0-Team_A/Python/FaceDepthAndExpressionRecognition.py
@@ -6,7 +6,7 @@ import sys
 
 cap = cv2.VideoCapture(0)
 detector = FaceMeshDetector(maxFaces=2)
-detected_emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+# detected_emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
 while True:
     success, img = cap.read()
@@ -41,23 +41,27 @@ while True:
                 if emotion == "fear":
                     print("fer{:.1f}".format(face_emotions["emotion"][emotion]))
                     sys.stdout.flush()
+                else:
+                    continue
 
                 if face_emotions["emotion"][emotion] > 80:
-                    colour = (0, 155, 255)
+                    colour = (0, 140, 255)
                 else:
-                    colour = (211, 211, 211)
+                    colour = (0, 220, 255)
 
-                msg = "{}: {:.1f}".format(detected_emotions[de_index], face_emotions["emotion"][emotion])
+                # msg = "{}: {:.1f}".format(detected_emotions[de_index], face_emotions["emotion"][emotion])
+                msg = "Fear: {:.1f}".format(face_emotions["emotion"][emotion])
                 cv2.putText(img, msg, (pointRight[0] + 40, pointRight[1] - 40 + de_index * 15),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 1, cv2.LINE_AA, )
                 de_index += 1
+                break
 
     if len(faces) > 1:
         cvzone.putTextRect(img, f'Depth Difference: {int(abs(faces_depth[0] - faces_depth[1]))}cm', (400, 50), scale=1, thickness=2)
         print("fd"+str(int(abs(faces_depth[0] - faces_depth[1]))))
         sys.stdout.flush()
 
-        if abs(faces_depth[0] - faces_depth[1]) < 50:
+        if abs(faces_depth[0] - faces_depth[1]) < 75:
             cv2.rectangle(img, (280, 230), (400, 280), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, 'Warning', (285, 265), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0, 0, 0), 2)
 
--- PFD-ATM_3.0-Team_A/wwwroot/js/site.js
+++ PFD-ATM_3.0-Team_A/wwwroot/js/site.js
@@ -18,31 +18,36 @@ setInterval( function () {
 }, 2500);
 
 function distanceWarning(d) {
-    if (d < 51 && d) {
-        $("#modal-title").text("Too close!");
-        $(".modal-body").html(' \
+    if (d < 76 && d) {
+        $("#faceDepthModalTitle").text("Too close!");
+        $("#faceDepthModalBody").html(' \
         <div class="text-center"> \
             <p>Please ensure you are at least 0.5m away from any individual.</p> \
             <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_6twxg3pm.json" background="transparent" speed="1" style="width: 300px; height: 300px; margin: 0 auto" loop autoplay></lottie-player> \
         </div > ');
-        $(".modal").modal("show");
+        $("#faceDepthModal").modal("show");
+    }
+    else {
+        $("#faceDepthModal").modal("hide");
     }
 }
 
 function facialExpressionCheck(em) {
-    if (em > 79) {
-        $("#modal-title").text("Do you require assistance?");
-        $(".modal-body").html(' \
-        <div class="text-center"> \
-            <p>Our system has detected that you are in some distress.</p> \
-            <p>Would you like to continue this trasaction?</p> \
-            <div class="d-sm-inline-flex"> \
-                <button type="button" class="btn btn-primary mx-2">End Transaction</button> \
-                <button type="button" onclick="modalHide()" class="btn btn-primary mx-2">Continue</button> \
-            </div > \
-        </div > ');
-        $(".modal").modal("show");
-    }
+    if (!$("#ferModal").is(":visible")) {
+        if (em > 79) {
+            $("#ferModalTitle").text("Do you require assistance?");
+            $("#ferModalBody").html(' \
+            <div class="text-center"> \
+                <p>Our system has detected that you are in some distress.</p> \
+                <p>Would you like to continue this trasaction?</p> \
+                <div class="d-sm-inline-flex"> \
+                    <button type="button" class="btn btn-primary mx-2">End Transaction</button> \
+                    <button type="button" onclick="modalHide()" class="btn btn-primary mx-2">Continue</button> \
+                </div > \
+            </div > ');
+            $("#ferModal").modal("show");
+        }
+    }   
 }
 
 function modalHide() {
