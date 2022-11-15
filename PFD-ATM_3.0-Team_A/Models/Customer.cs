f5da1ec5ce7203f6ebe49220846bb138749bb770
Ubertronian04 <S10223002@connect.np.edu.sg>
2022-11-15 19:44:14 +0800

    linked database and created customer model

---
+++ PFD-ATM_3.0-Team_A/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFD_ATM_3._0_Team_A.Models
+{
+    public class Customer
+    {
+        [StringLength(12, MinimumLength = 12)]
+        public string AccountNo { get; set; }
+        public string Name { get; set; }
+        [StringLength(9, MinimumLength = 9)]
+        public string NRIC { get; set; }
+        public string Contact { get; set; }
+        [StringLength(6, MinimumLength = 6)]
+        public string Pin { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Bal { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal WithdrawalLimit { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal TransferLimit { get; set; }
+    }
+}
