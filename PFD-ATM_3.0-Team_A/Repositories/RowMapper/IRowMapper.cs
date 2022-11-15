d45986ee6be158e456c068d8042129f07b1e2fbe
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-16 02:17:59 +0800

    Added base IRowMapper

---
+++ PFD-ATM_3.0-Team_A/Repositories/RowMapper/IRowMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace PFD_ATM_3._0_Team_A.Repositories.RowMapper
+{
+    public interface IRowMapper<MODEL>
+    {
+        public MODEL Convert(SqlDataReader reader);
+    }
+}
