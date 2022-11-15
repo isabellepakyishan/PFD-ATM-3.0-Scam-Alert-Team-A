8454c133a3b00bafc909e346780ef7f91908af87
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-16 02:18:35 +0800

    Rename Customer.cs to Accounts.cs

--- PFD-ATM_3.0-Team_A/Models/Accounts.cs
+++ PFD-ATM_3.0-Team_A/Models/Accounts.cs
@@ -6,20 +6,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PFD_ATM_3._0_Team_A.Models
 {
-    public class Customer
+    public class Accounts : IModel<string>
     {
+        [Display(Name = "Account Number")]
+        [Required(ErrorMessage = "Account Number must be 12 digits")]
         [StringLength(12, MinimumLength = 12)]
-        public string AccountNo { get; set; }
+        public string ID { get; set; }
+
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
         [StringLength(9, MinimumLength = 9)]
         public string NRIC { get; set; }
+
         public string Contact { get; set; }
+
         [StringLength(6, MinimumLength = 6)]
         public string Pin { get; set; }
+
         [DataType(DataType.Currency)]
-        public decimal Bal { get; set; }
+        public decimal Balance { get; set; }
+
         [DataType(DataType.Currency)]
         public decimal WithdrawalLimit { get; set; }
+
         [DataType(DataType.Currency)]
         public decimal TransferLimit { get; set; }
     }
