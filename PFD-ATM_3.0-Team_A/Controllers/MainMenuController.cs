5f62568b6688865c04bb4ca5389e3007bb7ce8d8
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-15 10:40:44 +0800

    Add MainMenu controller

---
+++ PFD-ATM_3.0-Team_A/Controllers/MainMenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
