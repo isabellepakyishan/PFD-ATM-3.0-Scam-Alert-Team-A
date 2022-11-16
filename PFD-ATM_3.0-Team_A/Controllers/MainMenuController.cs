8081cc483241d9377e1ac4a2d5f23fab152bba08
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-17 03:04:53 +0800

    Add CheckPin action to check the user-entered PIN number

--- PFD-ATM_3.0-Team_A/Controllers/MainMenuController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/MainMenuController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace PFD_ATM_3._0_Team_A.Controllers
 {
@@ -12,6 +14,38 @@ namespace PFD_ATM_3._0_Team_A.Controllers
         public ActionResult OtherServices()
         {
             return View();
+        }
+
+        public ActionResult CheckPIN(string pin)
+        {
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            return null;
         }
     }
 }
