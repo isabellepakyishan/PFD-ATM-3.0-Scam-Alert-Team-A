9fdf505e987bd0e95c87e5f528f05f323f52e1f0
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-17 03:06:50 +0800

    Add DispenseCashController to check the PIN number entered by the user based on various possibilities and case scenarios

---
+++ PFD-ATM_3.0-Team_A/Controllers/DispenseCashController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class DispenseCashController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public ActionResult WithdrawAmt(int? amount)
+        //{
+        //    Console.WriteLine(amount);
+        //    return null;
+        //}
+        public ActionResult CheckPIN()
+        {
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            return null;
+        }
+    }
+}
