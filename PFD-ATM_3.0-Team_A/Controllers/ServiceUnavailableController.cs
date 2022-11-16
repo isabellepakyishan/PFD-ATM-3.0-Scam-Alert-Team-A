301ed617ac8434c302e7ed7e5ca43f03bbf0dd8c
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-17 03:02:53 +0800

    Add ServiceUnavailableController

---
+++ PFD-ATM_3.0-Team_A/Controllers/ServiceUnavailableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class ServiceUnavailableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
