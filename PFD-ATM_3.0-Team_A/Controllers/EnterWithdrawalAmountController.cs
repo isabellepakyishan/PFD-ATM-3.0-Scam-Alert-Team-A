92d442388d0cf05eb6eceab166aaa8e1c1283a6e
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-17 03:04:20 +0800

    Add EnterWithdrawalAmount controller to authenticate users after they enter the amount which they intend to withdraw Included other possibilities where user enters a wrong PIN number and an extra * key to signify help needed

---
+++ PFD-ATM_3.0-Team_A/Controllers/EnterWithdrawalAmountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class EnterWithdrawalAmountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CheckPIN()
+        {
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            return null;
+        }
+    }
+}
