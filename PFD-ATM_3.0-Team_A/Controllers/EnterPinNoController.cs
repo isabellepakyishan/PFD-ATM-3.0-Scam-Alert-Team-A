b9402b2deeb214788f7b6a3f57bd129d8dee48aa
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-16 12:22:20 +0800

    Add session storage for PIN no Removed unnecessary using

--- PFD-ATM_3.0-Team_A/Controllers/EnterPinNoController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/EnterPinNoController.cs
@@ -1,16 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using PFD_ATM_3._0_Team_A.Models;
-using System;
-using System.Web;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using System.IO;
-using PFD_ATM_3._0_Team_A.Models;
 
 namespace PFD_ATM_3._0_Team_A.Controllers
 {
@@ -33,6 +22,7 @@ namespace PFD_ATM_3._0_Team_A.Controllers
             string pin7 = form["pin7"];
 
             string pinNo = pin1 + pin2 + pin3 + pin4 + pin5 + pin6 + pin7;
+            HttpContext.Session.SetString("PinNo", pinNo);
 
             return RedirectToAction("Index", "MainMenu");
         }
