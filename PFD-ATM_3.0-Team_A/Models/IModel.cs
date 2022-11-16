53d44ea6cdce6973e2f9e7340a85f83d97c98b29
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-16 11:31:45 +0800

    Removed base models, repositories and row mappers

--- PFD-ATM_3.0-Team_A/Models/IModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PFD_ATM_3._0_Team_A.Models
-{
-    public interface IModel<T>
-    {
-        public T ID { get; set; }
-    }
-}
--- PFD-ATM_3.0-Team_A/Repositories/RowMapper/IRowMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.SqlClient;
-
-namespace PFD_ATM_3._0_Team_A.Repositories.RowMapper
-{
-    public interface IRowMapper<MODEL>
-    {
-        public MODEL Convert(SqlDataReader reader);
-    }
-}
