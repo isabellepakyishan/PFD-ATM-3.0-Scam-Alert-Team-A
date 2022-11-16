e80d81a7e3e250b0757b1c7fef734c0dfef8d4ba
isabelle_pak <s10222456@connect.np.edu.sg>
2022-11-16 12:22:48 +0800

    Create AccountsDAL.cs

---
+++ PFD-ATM_3.0-Team_A/DAL/AccountsDAL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;   //Contains IConfiguration interface which reads JSON configuration information
+using System.IO;                            //Contains classes from file/directory input/output operations
+using System.Data.SqlClient;                //Contains ADO.NET classes like SqlConnection, SqlCommand, SqlDataReader
+using PFD_ATM_3._0_Team_A.Models;
+
+namespace PFD_ATM_3._0_Team_A.DAL
+{
+    public class AccountsDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor
+        public AccountsDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("PFDATM3.0TeamAConnectionString");
+
+            //Instantiate a SqlConnection object with the Connection String read. 
+            conn = new SqlConnection(strConn);
+        }
+
+        public bool IsAccountExist(string accountNo)
+        {
+            bool accountFound = false;
+
+            //Create a SqlCommand object and specify the SQL statement to get a staff record with the email address to be validated
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT AccountNo FROM Accounts WHERE AccountNo=@enteredAccount";
+            cmd.Parameters.AddWithValue("@enteredAccount", accountNo);
+
+            //Open a database connection and execute the SQL statement
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                //Records found
+                while (reader.Read())
+                {
+                    if (reader.GetString(0) == accountNo)
+                        //The email address is used by another staff
+                        accountFound = true;
+                }
+            }
+            else
+            {
+                //No record
+                accountFound = false; // The email address given does not exist
+            }
+
+            reader.Close();
+            conn.Close();
+
+            return accountFound;
+        }
+
+        //public Accounts GetAccount(string accountNo)
+        //{
+        //    //Create a SqlCommand object and specify the SQL statement to get a staff record with the email address to be validated
+        //    SqlCommand cmd = conn.CreateCommand();
+        //    cmd.CommandText = @"SELECT AccountNo FROM Accounts WHERE AccountNo=@enteredAccount";
+        //    cmd.Parameters.AddWithValue("@enteredAccount", accountNo);
+
+        //    //Open a database connection and execute the SQL statement
+        //    conn.Open();
+
+        //    SqlDataReader reader = cmd.ExecuteReader();
+
+        //    Accounts retrievedAccount = new Accounts();
+        //    while (reader.Read())
+        //    {        
+        //        retrievedAccount.AccountNo = reader.GetString(0);
+        //    }
+
+        //    return retrievedAccount;
+        //}
+    }
+}
