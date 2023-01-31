019b84fea2f1e9e0d0d0e73d9fff727634099354
isabelle_pak <s10222456@connect.np.edu.sg>
2023-01-31 19:26:28 +0800

    Add EnterTransferAccount controller to check whether the user account number exists in the Accounts database and store the account number and name of the account holder in session storage

---
+++ PFD-ATM_3.0-Team_A/Controllers/EnterTransferAccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PFD_ATM_3._0_Team_A.DAL;
+using PFD_ATM_3._0_Team_A.Models;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class EnterTransferAccountController : Controller
+    {
+        private AccountsDAL accountContext = new AccountsDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StoreTransferAccountNo(IFormCollection form)
+        {
+            string transferAccountNo = form["transferAccountNo"];
+            bool accountExists = accountContext.IsAccountExist(transferAccountNo);
+
+            if (transferAccountNo == HttpContext.Session.GetString("AccountNo"))
+            {
+                return RedirectToAction("Index", "EnterTransferAccount");
+            }
+            else if (accountExists)
+            {
+                Accounts transferAccount = accountContext.GetAccount(transferAccountNo);
+                HttpContext.Session.SetString("TransferAccountNo", transferAccountNo);
+                HttpContext.Session.SetString("TransferAccountName", transferAccount.Name);
+                return RedirectToAction("Index", "EnterTransferAmount");
+            }
+            else
+
+                return RedirectToAction("Index", "EnterTransferAccount");
+        }
+    }
+}
