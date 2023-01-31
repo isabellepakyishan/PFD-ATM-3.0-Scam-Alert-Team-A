b6a8e053574bf3ba4238c99ac7651cec4a8251e5
isabelle_pak <s10222456@connect.np.edu.sg>
2023-01-31 19:23:49 +0800

    Add OtherServices controller

---
+++ PFD-ATM_3.0-Team_A/Controllers/OtherServicesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class OtherServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult EnterTransferAccount()
+        //{
+        //    return RedirectToAction("Index", "EnterTransferAccount");
+        //}
+    }
+}
