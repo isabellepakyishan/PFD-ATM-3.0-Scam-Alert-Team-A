b67ec2cf42be0b5024796f3c961ba6bb053d4a4b
Gabriel-Koh <s10219353@connect.np.edu.sg>
2023-02-01 21:40:04 +0800

    Added pop up for money transfers to a new account for the first time and saves transaction into database

--- PFD-ATM_3.0-Team_A/Controllers/EnterTransferAmountController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/EnterTransferAmountController.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using PFD_ATM_3._0_Team_A.DAL;
+using PFD_ATM_3._0_Team_A.Models;
 
 namespace PFD_ATM_3._0_Team_A.Controllers
 {
     public class EnterTransferAmountController : Controller
     {
+        private TransferDAL TransferContext = new TransferDAL();
         public IActionResult Index()
         {
+
             return View();
         }
 
         public ActionResult StoreTransferAmount(IFormCollection form)
         {
+
             int transferAmount = Convert.ToInt32(form["transferAmount"]);
             HttpContext.Session.SetInt32("TransferAmount", transferAmount);
-            return RedirectToAction("Index", "ConfirmTransfer");
+
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            string transferaccountNo = HttpContext.Session.GetString("TransferAccountNo");
+            bool recoerdExist = TransferContext.IsRecordExist(accountNo, transferaccountNo);
+
+            if (recoerdExist == true)
+            {
+                return RedirectToAction("Index", "ConfirmTransfer");
+            }
+            else
+
+                return RedirectToAction("Index", "NewAccTransfer");
         }
     }
 }
