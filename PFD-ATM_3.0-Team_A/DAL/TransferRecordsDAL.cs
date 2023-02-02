d9d59cd0e114cb048a52a8001cd705983ae923e2
isabelle_pak <s10222456@connect.np.edu.sg>
2023-02-02 20:23:45 +0800

    Implement HasTransferOccurredBefore function to obtain boolean value to see if there have been any prior transfers between the account holder and the account number entered to be transferred to

--- PFD-ATM_3.0-Team_A/DAL/TransferRecordsDAL.cs
+++ PFD-ATM_3.0-Team_A/DAL/TransferRecordsDAL.cs
@@ -20,6 +20,32 @@ namespace PFD_ATM_3._0_Team_A.DAL
             conn = new SqlConnection(strConn);
         }
 
+        public bool HasTransferOccurredBefore(string accountNo, string transferAccountNo)
+        {
+            bool transferOccurred = false;
+
+            //Create a SqlCommand object and specify the SQL statement to get a staff record with the email address to be validated
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT COUNT(*) FROM TransferRecords WHERE AccountNo=@enteredAccount AND TransferAccountNo=@transferaccountNo";
+            cmd.Parameters.AddWithValue("@enteredAccount", accountNo);
+            cmd.Parameters.AddWithValue("@transferaccountNo", transferAccountNo);
+
+            //Open a database connection and execute the SQL statement
+            conn.Open();
+            int count = (int)cmd.ExecuteScalar();
+            conn.Close();
+
+            if (count == 0)
+            {
+                transferOccurred = false;
+            }
+            else
+            {
+                transferOccurred = true;
+            }
+            return transferOccurred;
+        }
+
         public int InsertTransferRecord(string accountNo, string transferAccountNo, decimal transferAmount, bool transferSuspicious)
         {
             SqlCommand cmd = conn.CreateCommand();
