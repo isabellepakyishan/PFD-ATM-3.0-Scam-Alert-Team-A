61dc5018c4f4d2857a1de979ce4651a54ba76539
isabelle_pak <s10222456@connect.np.edu.sg>
2023-02-02 20:28:53 +0800

    Add FirstFundsTransfer controller and ContinueToEnterTransferAmount and ReturnToEnterTransferAccount actions for if the user confirms he wants to transfer to this account or if he decides to cancel the transfer

---
+++ PFD-ATM_3.0-Team_A/Controllers/FirstFundsTransferController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PFD_ATM_3._0_Team_A.DAL;
+using PFD_ATM_3._0_Team_A.Models;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class FirstFundsTransferController : Controller
+    {
+        private AccountsDAL accountContext = new AccountsDAL();
+        private TransferDAL transferContext = new TransferDAL();
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult ContinueToEnterTransferAmount()
+        {
+            TempData["TransferAccountName"] = HttpContext.Session.GetString("TransferAccountName");
+            return RedirectToAction("Index", "EnterTransferAmount");
+        }
+
+        public ActionResult ReturnToEnterTransferAccount()
+        {
+            return RedirectToAction("Index", "EnterTransferAccount");
+        }
+    }
+}
