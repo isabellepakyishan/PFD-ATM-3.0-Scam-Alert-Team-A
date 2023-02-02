437b9a5f9db93c5a8386ac3c9b508426bd4858e8
isabelle_pak <s10222456@connect.np.edu.sg>
2023-02-02 16:14:09 +0800

    Implement CheckPIN50, CheckPIN80, CheckPIN100 and CheckPIN200 to enable fast cash withdrawals (but with the necessary checking still in place)

--- PFD-ATM_3.0-Team_A/Controllers/DispenseCashController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/DispenseCashController.cs
@@ -1,15 +1,226 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PFD_ATM_3._0_Team_A.DAL;
+using PFD_ATM_3._0_Team_A.Models;
 using System;
 
 namespace PFD_ATM_3._0_Team_A.Controllers
 {
     public class DispenseCashController : Controller
     {
+        private AccountsDAL accountContext = new AccountsDAL();
+        private WithdrawalRecordsDAL withdrawalContext = new WithdrawalRecordsDAL();
+        
         public IActionResult Index()
         {
             return View();
         }
+
+        public ActionResult CheckPIN50()
+        {
+            HttpContext.Session.SetString("TransactionType", "Withdrawal");
+            
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            Accounts retrievedAccount = accountContext.GetAccount(accountNo);
+            
+            int intendedWithdrawalAmount = 50;
+            decimal accountBalance = retrievedAccount.Balance;
+
+            HttpContext.Session.SetInt32("WithdrawalAmount", intendedWithdrawalAmount);
+
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    int timesWithdrawn = retrievedAccount.TimesWithdrawn;
+                    int newTimesWithdrawn = retrievedAccount.TimesWithdrawn += 1;
+                    decimal newAvgWithdrawal = (retrievedAccount.AvgWithdrawal * timesWithdrawn + intendedWithdrawalAmount) / newTimesWithdrawn;
+                    decimal finalBalance = retrievedAccount.Balance - intendedWithdrawalAmount;
+
+                    if (ModelState.IsValid)
+                    {
+                        accountContext.WithdrawalUpdateAccountDetails(accountNo, finalBalance, newAvgWithdrawal, newTimesWithdrawn);
+                        withdrawalContext.InsertWithdrawalRecord(accountNo, intendedWithdrawalAmount, false);
+                    }
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+        }
+
+        public ActionResult CheckPIN80()
+        {
+            HttpContext.Session.SetString("TransactionType", "Withdrawal");
+
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            Accounts retrievedAccount = accountContext.GetAccount(accountNo);
+
+            int intendedWithdrawalAmount = 80;
+            decimal accountBalance = retrievedAccount.Balance;
+
+            HttpContext.Session.SetInt32("WithdrawalAmount", intendedWithdrawalAmount);
+
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    int timesWithdrawn = retrievedAccount.TimesWithdrawn;
+                    int newTimesWithdrawn = retrievedAccount.TimesWithdrawn += 1;
+                    decimal newAvgWithdrawal = (retrievedAccount.AvgWithdrawal * timesWithdrawn + intendedWithdrawalAmount) / newTimesWithdrawn;
+                    decimal finalBalance = retrievedAccount.Balance - intendedWithdrawalAmount;
+
+                    if (ModelState.IsValid)
+                    {
+                        accountContext.WithdrawalUpdateAccountDetails(accountNo, finalBalance, newAvgWithdrawal, newTimesWithdrawn);
+                        withdrawalContext.InsertWithdrawalRecord(accountNo, intendedWithdrawalAmount, false);
+                    }
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+        }
+
+        public ActionResult CheckPIN100()
+        {
+            HttpContext.Session.SetString("TransactionType", "Withdrawal");
+
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            Accounts retrievedAccount = accountContext.GetAccount(accountNo);
+
+            int intendedWithdrawalAmount = 100;
+            decimal accountBalance = retrievedAccount.Balance;
+
+            HttpContext.Session.SetInt32("WithdrawalAmount", intendedWithdrawalAmount);
+
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    int timesWithdrawn = retrievedAccount.TimesWithdrawn;
+                    int newTimesWithdrawn = retrievedAccount.TimesWithdrawn += 1;
+                    decimal newAvgWithdrawal = (retrievedAccount.AvgWithdrawal * timesWithdrawn + intendedWithdrawalAmount) / newTimesWithdrawn;
+                    decimal finalBalance = retrievedAccount.Balance - intendedWithdrawalAmount;
+
+                    if (ModelState.IsValid)
+                    {
+                        accountContext.WithdrawalUpdateAccountDetails(accountNo, finalBalance, newAvgWithdrawal, newTimesWithdrawn);
+                        withdrawalContext.InsertWithdrawalRecord(accountNo, intendedWithdrawalAmount, false);
+                    }
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+        }
+
+        public ActionResult CheckPIN200()
+        {
+            HttpContext.Session.SetString("TransactionType", "Withdrawal");
+
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            Accounts retrievedAccount = accountContext.GetAccount(accountNo);
+
+            int intendedWithdrawalAmount = 200;
+            decimal accountBalance = retrievedAccount.Balance;
+
+            HttpContext.Session.SetInt32("WithdrawalAmount", intendedWithdrawalAmount);
+
+            var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+            var storedPin = HttpContext.Session.GetString("PinNo");
+
+            if (enteredPin.Length == 6)
+            {
+                if (enteredPin == storedPin)
+                {
+                    int timesWithdrawn = retrievedAccount.TimesWithdrawn;
+                    int newTimesWithdrawn = retrievedAccount.TimesWithdrawn += 1;
+                    decimal newAvgWithdrawal = (retrievedAccount.AvgWithdrawal * timesWithdrawn + intendedWithdrawalAmount) / newTimesWithdrawn;
+                    decimal finalBalance = retrievedAccount.Balance - intendedWithdrawalAmount;
+
+                    if (ModelState.IsValid)
+                    {
+                        accountContext.WithdrawalUpdateAccountDetails(accountNo, finalBalance, newAvgWithdrawal, newTimesWithdrawn);
+                        withdrawalContext.InsertWithdrawalRecord(accountNo, intendedWithdrawalAmount, false);
+                    }
+                    return RedirectToAction("Index", "DispenseCash");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+            else
+            {
+                char lastPin = enteredPin[enteredPin.Length - 1];
+                string first6Pin = enteredPin.Remove(6);
+                if (lastPin == '*' && first6Pin == storedPin)
+                {
+                    return RedirectToAction("Index", "AuthenticationError");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "IncorrectPinNo");
+                }
+            }
+        }
+
+
         //public ActionResult WithdrawAmt(int? amount)
         //{
         //    Console.WriteLine(amount);
