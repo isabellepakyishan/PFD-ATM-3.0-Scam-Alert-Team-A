de402c675ea51d1d878f645e2f9c1338b90f5e37
isabelle_pak <s10222456@connect.np.edu.sg>
2023-02-03 14:54:25 +0800

    Implement ReturnToHome to simulate secure log out from their account upon completing their funds transfer transaction

--- PFD-ATM_3.0-Team_A/Controllers/SuccessfulTransferController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/SuccessfulTransferController.cs
@@ -8,5 +8,10 @@ namespace PFD_ATM_3._0_Team_A.Controllers
         {
             return View();
         }
+
+        public ActionResult ReturnToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
