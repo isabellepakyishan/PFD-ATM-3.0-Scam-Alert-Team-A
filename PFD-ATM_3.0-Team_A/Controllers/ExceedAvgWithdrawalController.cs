823e11ab0835a8b6adef72264c4869124fecdf08
lanterex <s10204044@connect.np.edu.sg>
2023-02-03 16:41:05 +0800

    Added new controller

---
+++ PFD-ATM_3.0-Team_A/Controllers/ExceedAvgWithdrawalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFD_ATM_3._0_Team_A.Controllers
+{
+    public class ExceedAvgWithdrawalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
