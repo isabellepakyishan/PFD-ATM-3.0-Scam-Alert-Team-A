7cab8ff08f50909e67015aa951ace6910d4e0abb
isabelle_pak <s10222456@connect.np.edu.sg>
2023-02-03 18:59:46 +0800

    Implement BalanceEnquiry action to show user's their account balance conditionally based on their entered pin number

--- PFD-ATM_3.0-Team_A/Controllers/MainMenuController.cs
+++ PFD-ATM_3.0-Team_A/Controllers/MainMenuController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PFD_ATM_3._0_Team_A.DAL;
+using PFD_ATM_3._0_Team_A.Models;
 using System;
 
 namespace PFD_ATM_3._0_Team_A.Controllers
 {
     public class MainMenuController : Controller
     {
+        private AccountsDAL accountContext = new AccountsDAL();
+        private WithdrawalRecordsDAL withdrawalContext = new WithdrawalRecordsDAL(); 
+        
         public IActionResult Index()
         {
             return View();
@@ -16,16 +21,24 @@ namespace PFD_ATM_3._0_Team_A.Controllers
             return View();
         }
 
-        public ActionResult CheckPIN(string pin)
+        public ActionResult BalanceEnquiry()
         {
+            HttpContext.Session.SetString("TransactionType", "BalanceEnquiry");
+
             var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
             var storedPin = HttpContext.Session.GetString("PinNo");
+
+            string accountNo = HttpContext.Session.GetString("AccountNo");
+            Accounts retrievedAccount = accountContext.GetAccount(accountNo);
+            decimal accountBalance = Math.Round(retrievedAccount.Balance, 2);
+
+            HttpContext.Session.SetString("AccountBalance", Convert.ToString(accountBalance));
 
             if (enteredPin.Length == 6)
             {
                 if (enteredPin == storedPin)
                 {
-                    return RedirectToAction("Index", "DispenseCash");
+                    return RedirectToAction("Index", "BalanceEnquiry");
                 }
                 else
                 {
@@ -46,5 +59,36 @@ namespace PFD_ATM_3._0_Team_A.Controllers
                 }
             }
         }
+
+        //public ActionResult CheckPIN(string pin)
+        //{
+        //    var enteredPin = HttpContext.Session.GetString("EnteredPinNo");
+        //    var storedPin = HttpContext.Session.GetString("PinNo");
+
+        //    if (enteredPin.Length == 6)
+        //    {
+        //        if (enteredPin == storedPin)
+        //        {
+        //            return RedirectToAction("Index", "DispenseCash");
+        //        }
+        //        else
+        //        {
+        //            return RedirectToAction("Index", "IncorrectPinNo");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        char lastPin = enteredPin[enteredPin.Length - 1];
+        //        string first6Pin = enteredPin.Remove(6);
+        //        if (lastPin == '*' && first6Pin == storedPin)
+        //        {
+        //            return RedirectToAction("Index", "AuthenticationError");
+        //        }
+        //        else
+        //        {
+        //            return RedirectToAction("Index", "IncorrectPinNo");
+        //        }
+        //    }
+        //}
     }
 }
